d53668a1591c7b1fcfb917e51cb61e52cc33ca45
Zachary Metz <zach@tropicalgalaxy.io>
2024-10-22 10:47:24 -0600

    Initialize website

---
+++ precache-manifest.13ab28f747110f1198d4f5a5460a56b0.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "704f3dab549b2949464b7ad7e6471bf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "412cb4afcd795c4456fb",
+    "url": "/static/css/main.4f2547d4.chunk.css"
+  },
+  {
+    "revision": "76c20022e248a6381a8a",
+    "url": "/static/js/2.d924ebd6.chunk.js"
+  },
+  {
+    "revision": "412cb4afcd795c4456fb",
+    "url": "/static/js/main.da4aa26a.chunk.js"
+  },
+  {
+    "revision": "05c17d9cc4615ed25627",
+    "url": "/static/js/runtime-main.fd0ee7e7.js"
+  },
+  {
+    "revision": "8ebed5e74e881db8c13f7bd978b332dc",
+    "url": "/static/media/BAT.8ebed5e7.svg"
+  },
+  {
+    "revision": "d7876d48e80112e766d737346a616707",
+    "url": "/static/media/Bitcoin.d7876d48.svg"
+  },
+  {
+    "revision": "d47fb46925e90052a7a1f566812283b4",
+    "url": "/static/media/XRP.d47fb469.svg"
+  },
+  {
+    "revision": "f306cda7844d52b7a7d4101c6913726e",
+    "url": "/static/media/bank.f306cda7.svg"
+  },
+  {
+    "revision": "22a9cab5d1db019c872b32167fbe7345",
+    "url": "/static/media/betacoin.22a9cab5.png"
+  },
+  {
+    "revision": "32f9a94e5f4a6553c9a1a631963f1d13",
+    "url": "/static/media/betacoin1.32f9a94e.svg"
+  },
+  {
+    "revision": "1b33588cb8d8ab8674314aeb76a9fc38",
+    "url": "/static/media/chess-board.1b33588c.svg"
+  },
+  {
+    "revision": "605642c71463ca8ec99903719c98dcb0",
+    "url": "/static/media/dogecoin.605642c7.svg"
+  },
+  {
+    "revision": "81713350c953a254805db5a18317b396",
+    "url": "/static/media/eos.81713350.svg"
+  },
+  {
+    "revision": "18708d3e3f54116e2ea639fdd56dd6c5",
+    "url": "/static/media/ethereum.18708d3e.svg"
+  },
+  {
+    "revision": "a2b2bb8611d85e25370d6953ebf1ac7c",
+    "url": "/static/media/expand.a2b2bb86.svg"
+  },
+  {
+    "revision": "61583dbe807fd087e8f442311cce8cbf",
+    "url": "/static/media/fullscreen.61583dbe.svg"
+  },
+  {
+    "revision": "19072fe2a7429c24c3a634f7eb15fbe9",
+    "url": "/static/media/information-dark.19072fe2.svg"
+  },
+  {
+    "revision": "f99af8e9e241ae2288522f7d7c62089f",
+    "url": "/static/media/information.f99af8e9.svg"
+  },
+  {
+    "revision": "804ad7d2d3b7b825f8ddf781403c9ff6",
+    "url": "/static/media/isk.804ad7d2.png"
+  },
+  {
+    "revision": "c151518bf73d26f9286495ab4a6ea576",
+    "url": "/static/media/litecoin.c151518b.svg"
+  },
+  {
+    "revision": "f9caaf1e3ae748a086e6d84093600fe7",
+    "url": "/static/media/monero.f9caaf1e.svg"
+  },
+  {
+    "revision": "f8bbed125e99dfff710e739ee5750ef7",
+    "url": "/static/media/spring-swing-rocket.f8bbed12.svg"
+  }
+]);
